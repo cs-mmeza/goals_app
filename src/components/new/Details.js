495cddd11a0b600b231d34016bea0c695fae7e2e
cs-mmeza <mmeza1701@gmail.com>
2023-12-01 22:36:21 -0500

    add form style to add and create new goals.

--- src/components/new/Details.js
+++ src/components/new/Details.js
@@ -1,41 +1,51 @@
+import styles from "./Details.module.css";
 function Details() {
-    const icon = ["💻", "✈️", "⚽", "📚", "💵"];
+    const icon = ["💻", "✈️", "⚽", "📚", "💵", "🏃"];
     return (
-        <div>
-            <form>
-                <label>
+        <div className="card">
+            <form className="p-4">
+                <label className="label">
                     What's you new goal?
-                    <input placeholder="ex. run 62 times in a year"/>
+                    <input 
+                        className="input"
+                        placeholder="ex. run 62 times in a year"/>
                 </label>
-                <label>
+                <label className="label">
                     How long would it take in your own peace? <span>(ex. 1 time weekly)</span>
-                    <input type="number"/>
-                    <select>
-                        <option value="day">daily</option>
-                        <option value="week">weekly</option>
-                        <option value="month">monthly</option>
-                        <option value="year">yearly</option>
-                    </select>
+                    <div className="flex mb-6">
+                        <input 
+                            className="input mr-6"
+                            type="number"/>
+                        <select className="input">
+                            <option value="day">daily</option>
+                            <option value="week">weekly</option>
+                            <option value="month">monthly</option>
+                            <option value="year">yearly</option>
+                        </select>
+                    </div>  
                 </label>
-                <label>
+                <label className="label">
                     Do you have a deadline?
-                    <input type="date"/>
+                    <input 
+                        className="input"
+                        type="date"/>
                 </label>
-                <label>
+                <label className="label">
                     How many times have you achive this goal before?
-                    <input type="number" />
+                    <input 
+                        className="input"
+                        type="number" />
                 </label>
-                <label>
+                <label className="label">
                     You can chose the icon that you thing fits better this goal.
-                    <input/>
-                    <select>
-                        {icon.map(option => <option value={option}></option>)}
+                    <select className="input">
+                        {icon.map(option => <option value={option}>{option}</option>)}
                     </select>
                 </label>
             </form>
-            <div>
-                <button>Create</button>
-                <button>Cancel</button>
+            <div className={styles.buttons}>
+                <button className="button button--black">Create</button>
+                <button className="button button--gray">Cancel</button>
             </div>
         </div>
     );
